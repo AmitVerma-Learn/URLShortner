03f830645b1786c350b1668c987e38a09664f253
AmitVerma-Learn <bapista50@gmail.com>
2021-11-11 20:49:48 +0530

    comit

--- Encoder62.py
+++ Encoder62.py
@@ -1,8 +1,8 @@
-def base62_encoder(deci):
-  s = '012345689abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+def base62_encode(deci):
+  s='012345689abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
   hash_str = ''
   while deci > 0:
     hash_str= s[deci % 62] + hash_str
-    deci //=62
+    deci //= 62
   return hash_str
-print base62_encoder(999999)
+print(base62_encode(999999))
